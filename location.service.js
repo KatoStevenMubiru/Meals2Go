159e5452a1b0fe9e399879f27335428d86dce8b5
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-01-16 04:58:48 +0300

    Add files via upload

---
+++ location.service.js
@@ -0,0 +1,25 @@
+import camelize from "camelize";
+import { locations } from "./location.mock";
+
+export const locationRequest = (searchTerm) => {
+
+    return new Promise((resolve, reject) => {
+const locationMock = locations[searchTerm]
+if(!locationMock){
+    reject("not found");
+}
+resolve(locationMock);
+
+
+    })
+
+
+}
+export const locationTransform = () => {
+    const {geometry= {}} = camelize(result.results);
+  const {lat, lng} = geometry.location;
+
+  return{ lat, lng};
+
+    
+}
